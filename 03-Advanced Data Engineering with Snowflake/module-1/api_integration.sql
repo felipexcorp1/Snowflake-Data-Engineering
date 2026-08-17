20d1ebb93753c6780f014d37f76c0a8eecf8938e
felipexcorp1 <felipexcorp1@gmail.com>
2026-08-17 17:21:14 -0400

    Update API integration and database creation script

--- 03-Advanced Data Engineering with Snowflake/module-1/api_integration.sql
+++ 03-Advanced Data Engineering with Snowflake/module-1/api_integration.sql
@@ -1,28 +1,25 @@
 USE ROLE accountadmin;
-CREATE DATABASE course_repo;
+CREATE or replace DATABASE course_repo;
 USE SCHEMA public;
 
 -- Create credentials
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
   USERNAME = 'felipexcorp1'
-  PASSWORD = '';
+  PASSWORD = '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';
 
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION GIT_API_INTEGRATION_COURSE_3
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/felipexcorp1') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
-
--- List the git repositories
-SHOW GIT REPOSITORIES;
+  API_INTEGRATION =  GIT_API_INTEGRATION_COURSE_3-- Name of the API integration defined above
+  ORIGIN = 'https://github.com/felipexcorp1/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 SHOW GIT REPOSITORIES;
 
 SELECT
